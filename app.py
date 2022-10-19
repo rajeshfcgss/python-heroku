073815980d3940c2398cdb8e850fa6ecb8629322
rajeshfcgss <rajeshgunait@gmail.com>
2022-10-19 17:15:07 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, request
+
+import pywhatkit as py
+app = Flask(__name__)
+phone_number = ""
+message = ""
+time_min ='' 
+time_hour =''
+
+@app.route('/', methods=['GET',"POST"])
+
+def home():
+    if request.method == "POST":
+        phone_number = str(request.form.get('phone_number'))
+        message = str(request.form.get('message'))
+        time_min = int(request.form.get('time_min'))
+        time_hour = int(request.form.get('time_hour'))
+        py.sendwhatmsg(phone_number,message,time_hour, time_min)
+        return render_template('result.html', add=message)
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('port',8080)))
